90d51b9a0a427bb2def32db050943978fd5394bf
jackwang <wdmzjjm@163.com>
2016-08-03 00:31:01 +0800

    test

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -50,7 +50,7 @@ function getDefaultModules() {
       },
       {
         test: /\.(png|jpg|gif|woff|woff2|eot|ttf)$/,
-        loader: 'url-loader?limit=8192&name=../assets/[name].[ext]'
+        loader: 'url-loader?limit=8192'
       },
       {
         test: /\.(mp4|ogg|svg)$/,
